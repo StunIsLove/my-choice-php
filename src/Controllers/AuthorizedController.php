8c1e1fd54fa12c3a7ea472fd360dda0fc5279575
StunIsLove <s.koshevoy@billingonline.ru>
2022-04-07 23:18:20 +0300

    Added authorized controller

---
+++ src/Controllers/AuthorizedController.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class AuthorizedController extends ResponseController
+{
+    public function __init()
+    {
+
+    }
+}
--- src/Controllers/ResponseController.php
+++ src/Controllers/ResponseController.php
@@ -10,24 +10,57 @@ class ResponseController
      *
      * @return string
      */
-    public function returnSuccess(array $response): string
+    public static function returnSuccess(array $response): string
     {
-        return json_encode(['response' => $response]);
+        $result = json_encode(['response' => $response]);
+        echo $result;
+
+        return $result;
     }
 
     /**
-     * @param int $httpStatus
-     * @param     $errorMessage
+     * @param int    $httpStatus
+     * @param string $errorMessage
      *
      * @return string
      */
-    public function returnError(int $httpStatus, $errorMessage): string
+    public static function returnError(int $httpStatus, string $errorMessage): string
     {
         http_response_code($httpStatus);
 
-        return json_encode(array(
+        $result = json_encode(array(
             'error' => true,
             'errorMessage' => $errorMessage
         ));
+        echo $result;
+
+        return $result;
+    }
+
+    /**
+     * @return array|mixed
+     */
+    protected static function getInputData(): array
+    {
+        $body = file_get_contents('php://input');
+
+        $data = json_decode((string)$body, true);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            self::returnError(400, 'Json error: ' . json_last_error());
+        }
+
+        return $data ?? [];
+    }
+
+    /**
+     * @param string $headerName
+     *
+     * @return string|null
+     */
+    protected static function getHeader(string $headerName): ?string
+    {
+        $header = getallheaders()[$headerName] ?? null;
+
+        return $header ? (string)$header : null;
     }
 }
